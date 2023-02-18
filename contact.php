9edcda05492a794d1b9f646fb421aa4e532e9327
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2023-02-18 21:22:39 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,326 @@
+
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+
+<head>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- ==============================================
+		TITLE AND META TAGS
+		=============================================== -->
+		<title>Elow College of Professional Studies</title>
+        <meta name="description" content="www.elowcollege.co.ke , Elow college , colleges in keyna ,learning English in keyna  , learning English in nirobi ,learning Arabic  in keyna , ,learning french in nirobi ,ielts in south Africa ,learning Arabic in keyna  , study English in kenya ,
+
+
+">
+        <meta name="keywords" content="">
+        <meta name="author" content="Quickdev">
+
+		<!-- ==============================================
+		FAVICON
+		=============================================== -->
+        <link rel="shortcut icon" href="img/master/logo.png">
+
+		<!-- ==============================================
+		CSS
+		=============================================== -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/navbar.css">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/slick.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <script src="js/modernizr-custom.js"></script>
+
+
+
+<div class="top-header">
+        <div class="container">
+            <div class="top-header-left">
+                <div class="inner-header-left"><p> <a href="careers.php">info@elowcollege.co.ke</a> </p></div>
+
+            </div>
+            <div class="top-header-social">
+                <div class="inner-header-social"><a href="https://www.facebook.com/100655702679219/posts/pfbid0WmH3iCjhRvkQFz23xMVLaysZFAjG2rXe2Z3gzVy8Unh94ULWRWcvvcT7Qn3X8jYrl/"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+                <div class="inner-header-social"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></div>
+                <div class="inner-header-social"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></div>
+                <div class="inner-header-social"><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></div>
+            </div>
+        </div>
+    </div>
+   <header>
+         <div class="container">
+              <div class="header-left">
+                  <figure class="brand"><a href="index.php"><img src="img/master/logo.png" style="height:162px ; width:180px;margin-top:-26.5%" alt=""></a></figure>
+              </div>
+              <div class="top-location">
+                  <div class="address">
+                       <h6 style="color:#002db3"><b>Location:</b></h6>
+                      <p style="color:black">Nairobi , Kenya</p>
+                  </div>
+                  <div class="email">
+                       <h6 style="color:#002db3"  ><b>Email Us:</b></h6>
+                      <p style="color:black">info@elowcollege.co.ke</p>
+                  </div>
+                  <div class="phone">
+                       <h6  style="color:#002db3"><b>Call Us:</b></h6>
+                      <p style="color:black">+254750900650</p>
+                  </div>
+              </div>
+          </div>
+      </header>
+
+       <nav>
+           <div class="container">
+               <nav class="navbar navbar-expand-lg">
+                 <a class="navbar-brand" href="index.php"><div class="logo-brand"><img src="img/master/logo.png" alt=""></div></a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span>
+                 </button>
+
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto"  style=" margin-top:-1%">
+                  <li class="nav-item dropdown">
+  <a class="nav-link
+                    " href="index.php"  style="font-family: sans-serif;  font-size:19px" ><span style=" text-transform:uppercase;font-size:24px">H</span>ome </a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link
+                    " href="about.php"  style="font-family: sans-serif;  font-size:19px" ><span style="  ;font-size:24px">A</span>bout </a>
+                  </li>
+                  <li class="nav-item">
+  <a class="nav-link
+                    " href="cources.php" style="font-family: sans-serif;  font-size:19px"  ><span style=" text-transform:uppercase;font-size:24px">P</span>rograms </a>
+
+                  </li>
+                   <li class="nav-item">
+				    <a class="nav-link
+				                      " href="news.php" style="font-family: sans-serif;  font-size:19px"    ><span style=" text-transform:uppercase;font-size:24px">N</span>ews </a>
+
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link
+                    " href="team.php"  style="font-family: sans-serif;  font-size:19px"   ><span style=" text-transform:uppercase;font-size:24px">T</span>eam </a>
+                  </li>
+
+
+
+  <li class="nav-item">
+                     <a class="nav-link
+                    " href="portfolio.php"  style="font-family: sans-serif;  font-size:19px"   ><span style=" text-transform:uppercase;font-size:24px">P</span>ortfolio </a>
+                  </li>
+
+   <li  class="nav-item">
+                    <a class="nav-link
+                    " href="contact.php"  style="font-family: sans-serif;  font-size:19px"   ><span style=" text-transform:uppercase;font-size:24px">c</span>ontact</a>
+
+                  </li>
+
+
+
+ <li  class="nav-item">
+                    <a class="nav-link
+                    " href="application.php"  style="font-family: sans-serif;  font-size:19px"    target="_blank"  ><span style=" text-transform:uppercase;font-size:24px">A</span>pplication</a>
+
+                  </li>
+
+
+
+				   <li  class="nav-item">
+				                      <a class="nav-link
+				                      " href="index-ar.php"   style="font-family: sans-serif;  font-size:19px"   ><span style="  color: #cb8e01 ;text-transform:uppercase;font-size:24px">ar</span> </a>
+
+                  </li>
+                  </ul>
+               </nav>
+           </div>
+       </nav>
+
+
+ <body>
+
+    <div class="sections-contact">
+        <div class="maps">  
+        <iframe width="100%"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.6403802698405!2d36.82506269080327!3d-1.2816331270115362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1745e9bd68524e44!2zMcKwMTYnNTYuNyJTIDM2wrA0OScyMS41IkU!5e0!3m2!1sar!2s!4v1664524062527!5m2!1sar!2s"  height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>     
+
+
+</div>
+        
+        
+      
+
+        <div class="container">
+            <div class="pages-title">
+                <h1>Contact</h1>
+                <p>Home &nbsp; > &nbsp; Contact</p>
+            </div>
+        </div>
+    </div>
+
+	<section id="contact-page" class="pt-90 pb-120 gray-bg">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-lg-8">
+	                    <div class="contact-from">
+	                        <div class="section-title">
+	                            <h5 style="color: #1a53ff;">Contact Us</h5>
+	                            <h3 style=" text-transform:lowercase">If you have any questions , contact us </h3>
+	                        </div> <!-- section title -->
+	                        <div class="main-form pt-45">
+	                            <form  action="sendmail.php" method="post"  >
+	                                <div class="row">
+	                                    <div class="col-md-6">
+	                                        <div class="singel-form form-group">
+	                                            <input name="name" type="text" placeholder="Your name"   style="border: 1px solid #002db3;"  pattern="[A-Za-z].{1,}" required>
+	                                            <div class="help-block with-errors"></div>
+	                                        </div> <!-- singel form -->
+	                                    </div>
+	                                    <div class="col-md-6">
+	                                        <div class="singel-form form-group">
+	                                            <input name="email" type="email" placeholder="Email"   style="border: 1px solid #002db3;" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
+	                                            <div class="help-block with-errors"></div>
+	                                        </div> <!-- singel form -->
+	                                    </div>
+	                                    <div class="col-md-6">
+	                                        <div class="singel-form form-group">
+	                                            <input name="subject" type="text" placeholder="Subject"   style="border: 1px solid #002db3;"  style="width:200px"     pattern="[A-Za-z].{2,}" required>
+	                                            <div class="help-block with-errors"></div>
+	                                        </div> <!-- singel form -->
+	                                    </div>
+
+	                                    <div class="col-md-12">
+	                                        <div class="singel-form form-group">
+	                                            <textarea name="message" placeholder="Messege"  style="border: 1px solid #002db3;"  pattern="[A-Za-z].{2,}" required></textarea>
+	                                            <div class="help-block with-errors"></div>
+	                                        </div> <!-- singel form -->
+	                                    </div>
+	                                    <div class="col-md-12">
+	                                        <div class="singel-form">
+	                                        			           <div class="btn-send"><p><input type="submit" class="btn btn-custom btn-custom-small"  style="color:#fff" value="SEND MESSAGE"></p></div>
+  	                                        </div> <!-- singel form -->
+	                                    </div>
+	                                </div> <!-- row -->
+	                            </form>
+	                        </div> <!-- main form -->
+	                    </div> <!--  contact from -->
+	                </div>
+	                <div class="col-lg-4">
+	                    <div class="contact-address">
+	                        <div class="contact-heading">
+	                            <br>   <br>
+	                            <br>   <br>
+	                        </div>
+	                         <div class="contact-information"  >
+						                   <p class="address" style="color:black">  Nairobi Kenya,  at princely house second floor,door no;018</p>
+						                     <p   style="  color:black">+254750900650 - </p>
+						                     <p   style="  color:black">info@elowcollege.co.ke - </p>
+						                     <hr  style="  color:black">
+						                     <div class="social-contact" style="color:#002db3"><a href="https://www.facebook.com/100655702679219/posts/pfbid0WmH3iCjhRvkQFz23xMVLaysZFAjG2rXe2Z3gzVy8Unh94ULWRWcvvcT7Qn3X8jYrl/"><i class="fa fa-facebook" style="  color:black" aria-hidden="true"></i></a></div>
+						                     <div class="social-contact" style="color:#002db3"><a href="#"><i class="fa fa-twitter" style="  color:black" aria-hidden="true"></i></a></div>
+						                     <div class="social-contact" style="color:#002db3"><a href="#"><i class="fa fa-instagram" style="  color:black" aria-hidden="true"></i></a></div>
+						                     <div class="social-contact" style="color:#002db3"><a href="#"><i class="fa fa-rss" style="  color:black" aria-hidden="true"></i></a></div>
+                  </div>
+
+	                    </div> <!-- contact address -->
+
+	                </div>
+	            </div> <!-- row -->
+	        </div> <!-- container -->
+
+	    </section>
+   <section class="">
+        <!-- Footer -->
+   <footer class="text-center text-white" style="background-color: #0a4275;">
+       <!-- Grid container -->
+       <div class="container">
+        <div class="row">
+		              <div class="col-lg-4">
+		                <div class="footer-col">
+ 		                            <h5 > Elow college</h5>
+		                  <p> Elow college of professional studies is a tertiary college approved by the Kenyan ministry of education and TVETA to provide both certificate
+                 and diploma courses in various fields of professionalism . </p>
+		                 </div>
+
+		              </div>
+		              <div class="col-lg-2">
+		                <div class="footer-col">
+		                    <h5>POPULAR SITES</h5>
+		                    <ul class="list-group list-group-flush">
+		                        <li class="list-group-item">    <a href="about.php">About us</a></li>
+		                        <li class="list-group-item">  <a href="cources.php">Cources</a></li>
+		                        <li class="list-group-item"> <a href="portfolio.php">portfolio</a></li>
+		                         <li class="list-group-item">  <a href="contact.php">contact</a></li>
+		                    </ul>
+		                </div>
+		              </div>
+		              <div class="col-lg-2">
+		                <div class="footer-col">
+		                    <h5>LOCATION</h5>
+		                    <div class="location-box">
+ 		                        <h6>Location:</h6>
+		                        <p> Nairobi , Kenya</p>
+		                    </div>
+
+		                    <div class="location-box location-phone">
+ 		                        <h6>Call us:</h6>
+		                        <p>+254750900650</p>
+		                    </div>
+		                </div>
+		              </div>
+		              <div class="col-lg-4">
+		                <div class="footer-col last-col">
+		                    <h5>NEWSLETTER</h5>
+		                    <p>Suscribe to our newsletter and get the lastest scoop right to your inbox!</p>
+		                    <div class="newsletter-box">
+		                     <form  action="#" method="post" name="sign-up">
+		                       
+		                          <input type="email" class="input" id="eemail" name="email" placeholder="Your email address" required>
+  		                        <input type="submit" class="button" id="submit" value="SIGN UP">
+		                      </form>
+		                    </div>
+		                    <p  >Your email is safe with us </p>
+		                </div>
+		              </div>
+		            </div>
+		            <hr class="footer">
+		            <div class="bottom-footer">
+
+		                <div class="social-footer">
+		                    <div class="social-items"><a href="#"><div class="icon-fa"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></div>
+		                    <div class="social-items"><a href="#"><div class="icon-fa"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></div>
+		                    <div class="social-items"><a href="#"><div class="icon-fa"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></div>
+ 		                </div>
+		                
+            </div>
+       </div>
+       <!-- Grid container -->
+<br>
+       <!-- Copyright -->
+       <div class="text-center p-3" style="background-color:  #fff;color:  #002db3;">
+
+         <a class="text-white;" href="https://wwww.elowcollege.com.ke/" style= "color:#002db3;text-align:center; ">© Copyright elowcollege. All Rights Reserved</a>
+       </div>
+       <!-- Copyright -->
+     </footer>
+    <a href="#0" class="cd-top">Top</a>
+	<!-- ==============================================
+	JAVASCRIPTS
+	=============================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/loader.js"></script>
+    <script src="js/top.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/validator.js"></script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','js/analytics.js','ga');
+      ga('create', 'UA-101241150-1', 'auto');
+      ga('send', 'pageview');
+    </script>
